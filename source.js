86d3362f61d753a72dbf396dd10cd96947eb981f
Hasan Burak Songur <hasanburaksongur@gmail.com>
2023-02-20 11:23:25 +0300

    Finish the game when point reaches 5

--- source.js
+++ source.js
@@ -1,75 +1,82 @@
-function getComputerChoice() {
-    // Generate a random number between 0 and 2
-    const randomNumber = Math.floor(Math.random() * 3);
-  
-    // Map the random number to a choice
-    if (randomNumber === 0) {
-      return "rock";
-    } else if (randomNumber === 1) {
-      return "paper";
-    } else {
-      return "scissors";
-    }
-  }
-let computerChoice = getComputerChoice()
-// console.log(computerChoice)
-/*
-  const computerChoice = getComputerChoice();
-console.log(computerChoice);
-// Output: "rock", "paper", or "scissors"
-*/
-
-// create a function for user choice
-function getUserChoice() {
-    const userIn = prompt("Enter a number between 1 to 3:");
-    const userNum = parseInt(userIn);
-
-    // Map the user number to a choice
-    if (userNum === 1) {
-      return "rock";
-    } else if (userNum === 2) {
-      return "paper";
-    } else if (userNum === 3) {
-        return "scissors";
-    } else {
-      return "Wrong input. Please select a number not greater than 3!";
-    }
-  }
-let userChoice = getUserChoice()
-// console.log(userChoice)
-
-// create a score for computer and user
 let userScore = 0;
 let computerScore = 0;
 
-// compare the result with the computer
-function compareResults() {
-    if (userChoice === computerChoice){
-        computerScore = (computerScore + 1);
-        userScore = (userScore + 1)
+// create a repeated loop
+while (userScore < 5 && computerScore < 5) { 
+        function getComputerChoice() {
+            // Generate a random number between 0 and 2
+            const randomNumber = Math.floor(Math.random() * 3);
+        
+            // Map the random number to a choice
+            if (randomNumber === 0) {
+            return "rock";
+            } else if (randomNumber === 1) {
+            return "paper";
+            } else {
+            return "scissors";
+            }
+        }
+        let computerChoice = getComputerChoice()
+        // console.log(computerChoice)
+        /*
+        const computerChoice = getComputerChoice();
+        console.log(computerChoice);
+        // Output: "rock", "paper", or "scissors"
+        */
+        
+        // create a function for user choice
+        function getUserChoice() {
+            let userInput = prompt("Enter your selection:");
+            // Make the user input case-insensitive
+            userInput = userInput.toLowerCase();
+            if (userInput === "rock") {
+            return "rock";
+            } else if (userInput === "paper") {
+            return "paper";
+            } else if (userInput === "scissors") {
+                return "scissors";
+            } else {
+            alert ( "Wrong input. Please make your selection between the three items!");
+            }
+        }
+        let userChoice = getUserChoice()
+        //console.log(userChoice)    
+        // compare the result with the computer and add one point to who wins
+        // report who wins the round
+        function compareResults() {
+            if (userChoice === computerChoice){
+                console.log("It is a tie!")
+            }
+            else if (userChoice === "rock" && computerChoice === "paper") {
+                computerScore = (computerScore + 1);
+                console.log("You Lose! Paper beats rock")
+            }
+            else if (userChoice === "rock" && computerChoice === "scissors") {
+                userScore = (userScore + 1);
+                console.log("You Win! Rock beats scissors")
+            }
+            else if (userChoice === "paper" && computerChoice === "scissors") {
+                computerScore = (computerScore + 1);
+                console.log("You Lose! Scissors beats paper")
+            }
+            else if (userChoice === "paper" && computerChoice === "rock") {
+                userScore = (userScore + 1);
+                console.log("You Win! Paper beats rock")
+            }
+            else if (userChoice === "scissors" && computerChoice === "rock") {
+                computerScore = (computerScore + 1)
+                console.log("You Lose! Rock beats scissors")
+            }
+            else if (userChoice === "scissors" && computerChoice === "paper") {
+                userScore = (userScore + 1);
+                console.log("You Win! Scissors beats paper")
+            }
+        }
+        compareResults() ;
     }
-    else if (userChoice === "rock" && computerChoice === "paper") {
-        computerScore = (computerScore + 1)
-    }
-    else if (userChoice === "rock" && computerChoice === "scissors") {
-        userScore = (userScore + 1)
-    }
-    else if (userChoice === "paper" && computerChoice === "scissors") {
-        computerScore = (computerScore + 1)
-    }
-    else if (userChoice === "paper" && computerChoice === "rock") {
-        userScore = (userScore + 1)
-    }
-    else if (userChoice === "scissors" && computerChoice === "rock") {
-        computerScore = (computerScore + 1)
-    }
-    else if (userChoice === "scissors" && computerChoice === "paper") {
-        userScore = (userScore + 1)
-    }
-}
-compareResults();
-// console.log(userScore)
-// console.log(computerScore)
-
-// add one point to who wins
 // finish the game when some one reaches to five point
+if (userScore === 5){
+      alert("You Win!!!! Congrats!!!")
+    } else if (computerScore === 5){
+      alert("You Loose. Try Next Time.")
+    }
