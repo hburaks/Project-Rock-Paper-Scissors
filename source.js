bbfd3def6d9f00cbc862cfc592b822f47cc5bee5
Hasan Burak Songur <hasanburaksongur@gmail.com>
2023-02-27 11:21:43 +0300

    Update js and html code to get user input from button

--- source.js
+++ source.js
@@ -1,82 +1,152 @@
 let userScore = 0;
 let computerScore = 0;
+const scissors = document.querySelector('#scissors');
+const rock = document.querySelector('#rock');
+const paper = document.querySelector('#paper');
+const buttons = document.querySelectorAll("button");
 
-// create a repeated loop
-while (userScore < 5 && computerScore < 5) { 
-        function getComputerChoice() {
-            // Generate a random number between 0 and 2
-            const randomNumber = Math.floor(Math.random() * 3);
-        
-            // Map the random number to a choice
-            if (randomNumber === 0) {
-            return "rock";
-            } else if (randomNumber === 1) {
-            return "paper";
-            } else {
-            return "scissors";
-            }
+
+function getComputerChoice() {
+    const choices = ["rock", "paper", "scissors"];
+    const randomNumber = Math.floor(Math.random() * 3);
+    return choices[randomNumber];
+}
+
+buttons.forEach(button => {
+    button.addEventListener("click", () => {
+        if (button === scissors) {
+            userChoice= "scissors";
+        } else if (button === rock) {
+            userChoice= "rock";
+        } else if (button === paper) {
+            userChoice= "paper";
         }
-        let computerChoice = getComputerChoice()
-        // console.log(computerChoice)
-        /*
-        const computerChoice = getComputerChoice();
-        console.log(computerChoice);
-        // Output: "rock", "paper", or "scissors"
-        */
-        
-        // create a function for user choice
-        function getUserChoice() {
-            let userInput = prompt("Enter your selection:");
-            // Make the user input case-insensitive
-            userInput = userInput.toLowerCase();
-            if (userInput === "rock") {
-            return "rock";
-            } else if (userInput === "paper") {
-            return "paper";
-            } else if (userInput === "scissors") {
-                return "scissors";
-            } else {
-            alert ( "Wrong input. Please make your selection between the three items!");
-            }
+        computerChoice = getComputerChoice();
+        compareResults(computerChoice);
+
+        if (userScore === 5){
+            alert(`You Win!!!! Congrats!!! User: ${userScore} Computer: ${computerScore}`);
+            resetScores();
+        } else if (computerScore === 5){
+            alert(`You Lose. Try Next Time. User: ${userScore} Computer: ${computerScore}`);
+            resetScores();
         }
-        let userChoice = getUserChoice()
-        //console.log(userChoice)    
-        // compare the result with the computer and add one point to who wins
-        // report who wins the round
-        function compareResults() {
-            if (userChoice === computerChoice){
-                console.log("It is a tie!")
-            }
-            else if (userChoice === "rock" && computerChoice === "paper") {
-                computerScore = (computerScore + 1);
-                console.log("You Lose! Paper beats rock")
-            }
-            else if (userChoice === "rock" && computerChoice === "scissors") {
-                userScore = (userScore + 1);
-                console.log("You Win! Rock beats scissors")
-            }
-            else if (userChoice === "paper" && computerChoice === "scissors") {
-                computerScore = (computerScore + 1);
-                console.log("You Lose! Scissors beats paper")
-            }
-            else if (userChoice === "paper" && computerChoice === "rock") {
-                userScore = (userScore + 1);
-                console.log("You Win! Paper beats rock")
-            }
-            else if (userChoice === "scissors" && computerChoice === "rock") {
-                computerScore = (computerScore + 1)
-                console.log("You Lose! Rock beats scissors")
-            }
-            else if (userChoice === "scissors" && computerChoice === "paper") {
-                userScore = (userScore + 1);
-                console.log("You Win! Scissors beats paper")
-            }
-        }
-        compareResults() ;
+    });
+});
+
+function compareResults() {
+    if (userChoice === computerChoice){
+        console.log("It is a tie!")
+        alert('It is a tie!');
     }
-// finish the game when some one reaches to five point
-if (userScore === 5){
-      alert("You Win!!!! Congrats!!!")
-    } else if (computerScore === 5){
-      alert("You Loose. Try Next Time.")
+    else if (userChoice === "rock" && computerChoice === "paper") {
+        computerScore++;
+        console.log("You Lose! Paper beats rock")
     }
+    else if (userChoice === "rock" && computerChoice === "scissors") {
+        userScore++;
+        console.log("You Win! Rock beats scissors")
+    }
+    else if (userChoice === "paper" && computerChoice === "scissors") {
+        computerScore++;
+        console.log("You Lose! Scissors beats paper")
+    }
+    else if (userChoice === "paper" && computerChoice === "rock") {
+        userScore++;
+        console.log("You Win! Paper beats rock")
+    }
+    else if (userChoice === "scissors" && computerChoice === "rock") {
+        computerScore++;
+        console.log("You Lose! Rock beats scissors")
+    }
+    else if (userChoice === "scissors" && computerChoice === "paper") {
+        userScore++;
+        console.log("You Win! Scissors beats paper")
+    }
+    alert(`User: ${userScore} Computer: ${computerScore}`);
+}
+function resetScores() {
+    userScore = 0;
+    computerScore = 0;
+}
+
+
+
+
+
+/* buttons.forEach(button => {
+    button.addEventListener("click", () => {
+    scissors.addEventListener('click', function() {
+        userChoice= "scissors";
+    })
+    paper.addEventListener('click', function() {
+    })
+    rock.addEventListener('click', function() {
+        userChoice= "rock";
+    })
+    function getComputerChoice() {
+        // Generate a random number between 0 and 2
+        const randomNumber = Math.floor(Math.random() * 3);
+    
+        // Map the random number to a choice
+        if (randomNumber === 0) {
+        return "rock";
+        } else if (randomNumber === 1) {
+        return "paper";
+        } else {
+        return "scissors";
+        }
+    }
+    let computerChoice = getComputerChoice()
+    // compare the result with the computer and add one point to who wins
+    // report who wins the round
+    function compareResults() {
+        if (userChoice === computerChoice){
+            console.log("It is a tie!")
+            alert('It is a tie!');
+        }
+        else if (userChoice === "rock" && computerChoice === "paper") {
+            computerScore = (computerScore + 1);
+            console.log("You Lose! Paper beats rock")
+        }
+        else if (userChoice === "rock" && computerChoice === "scissors") {
+            userScore = (userScore + 1);
+            console.log("You Win! Rock beats scissors")
+        }
+        else if (userChoice === "paper" && computerChoice === "scissors") {
+            computerScore = (computerScore + 1);
+            console.log("You Lose! Scissors beats paper")
+        }
+        else if (userChoice === "paper" && computerChoice === "rock") {
+            userScore = (userScore + 1);
+            console.log("You Win! Paper beats rock")
+        }
+        else if (userChoice === "scissors" && computerChoice === "rock") {
+            computerScore = (computerScore + 1)
+            console.log("You Lose! Rock beats scissors")
+        }
+        else if (userChoice === "scissors" && computerChoice === "paper") {
+            userScore = (userScore + 1);
+            console.log("You Win! Scissors beats paper")
+        }
+        alert("User: ${userScore} Computer: ${computerScore}")
+        compareResults();
+        alert(userScore)
+        alert(computerScore)
+        
+
+        if (userScore === 5){
+            alert("You Win!!!! Congrats!!! User: ${userScore} Computer: ${computerScore}")
+            } else if (computerScore === 5){
+            alert("You Loose. Try Next Time. User: ${userScore} Computer: ${computerScore}")
+            }
+        }
+    });
+});
+
+
+
+ 
+
+
+// finish the game when some one reaches to five point */
